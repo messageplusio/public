9c875156cebf35b659637a1482b49b0084d6e9e5
Ali Orujaliyev <orujaliyevali@gmail.com>
2025-06-23 20:25:51 +0400

    new tag push

--- wawidget.js
+++ wawidget.js
@@ -1,7 +1,7 @@
-import { checkUserCountry } from 'https://cdn.jsdelivr.net/gh/messageplusio/public@latest/geoLocation.js';
-import { isVisibleTime } from 'https://cdn.jsdelivr.net/gh/messageplusio/public@latest/widgetScheduler.js';
-import { applyStyles } from 'https://cdn.jsdelivr.net/gh/messageplusio/public@latest/styles.js';
-import { initWidget } from 'https://cdn.jsdelivr.net/gh/messageplusio/public@latest/widgetRenderer.js';
+import { checkUserCountry } from 'https://cdn.jsdelivr.net/gh/messageplusio/public@v1.0.0/geoLocation.js';
+import { isVisibleTime } from 'https://cdn.jsdelivr.net/gh/messageplusio/public@v1.0.0/widgetScheduler.js';
+import { applyStyles } from 'https://cdn.jsdelivr.net/gh/messageplusio/public@v1.0.0/styles.js';
+import { initWidget } from 'https://cdn.jsdelivr.net/gh/messageplusio/public@v1.0.0/widgetRenderer.js';
 
 export async function createWhatsappChatWidget(
   options = {
