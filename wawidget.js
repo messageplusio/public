50633790d40ed079140767cc7140515d0ce0afbb
Imran Hajiyev <haciyev008@gmail.com>
2025-07-08 19:55:51 +0400

    feature: added real time updates to translations and button/qr for resizing; removed console errors

--- wawidget.js
+++ wawidget.js
@@ -1,36 +1,47 @@
-import { checkUserCountry } from 'https://wawidgetstorage.blob.core.windows.net/widget-assets/geoLocation.js';
-import { isVisibleTime } from 'https://wawidgetstorage.blob.core.windows.net/widget-assets/widgetScheduler.js';
-import { applyStyles } from 'https://wawidgetstorage.blob.core.windows.net/widget-assets/styles.js';
-import { initWidget } from 'https://wawidgetstorage.blob.core.windows.net/widget-assets/widgetRenderer.js';
-import { detectLocale } from 'https://wawidgetstorage.blob.core.windows.net/widget-assets/detectLocale.js';
+import { checkUserCountry } from "https://wawidgetstorage.blob.core.windows.net/widget-assets/geoLocation.js";
+import { isVisibleTime } from "https://wawidgetstorage.blob.core.windows.net/widget-assets/widgetScheduler.js";
+import { applyStyles } from "https://wawidgetstorage.blob.core.windows.net/widget-assets/styles.js";
+import {
+  configureWidget,
+  initWidget,
+} from "https://wawidgetstorage.blob.core.windows.net/widget-assets/widgetRenderer.js";
+import { detectLocale } from "https://wawidgetstorage.blob.core.windows.net/widget-assets/detectLocale.js";
+
+const debounce = (func, wait) => {
+  let timeout;
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func(...args), wait);
+  };
+};
 
 export async function createWhatsappChatWidget(
   options = {
     enabled: false,
     autoShow: true,
-    locale: 'en',
+    locale: "en",
     businessSettings: {
       translations: {},
-      phoneNumber: '33634674038',
-      allowedCountries: ['FR', 'AZ'],
-      timeZone: 'Europe/Paris',
-      offDayStart: 'Fri',
-      offTimeStart: '17:00',
-      offDayEnd: 'Mon',
-      offTimeEnd: '09:00',
+      phoneNumber: "33634674038",
+      allowedCountries: ["FR", "AZ"],
+      timeZone: "Europe/Paris",
+      offDayStart: "Fri",
+      offTimeStart: "17:00",
+      offDayEnd: "Mon",
+      offTimeEnd: "09:00",
     },
     displaySettings: {
-      backgroundColor: '#25D366',
+      backgroundColor: "#25D366",
       showToast: false,
       translations: {},
       showCTA: false,
       verticalMargin: 24,
       horizontalMargin: 24,
-      position: 'right'
-    }
-  }
+      position: "right",
+    },
+  },
 ) {
-  if (options.enabled === false) return
+  if (options.enabled === false) return;
 
   // CHECK COUNTRY RESTRICTIONS
   if (options.businessSettings.allowedCountries) {
@@ -39,19 +50,54 @@ export async function createWhatsappChatWidget(
   }
 
   // CHECK TIME VISIBILITY:
-  // check if widget should be shown based on day and time
-  // don't show widget if within off period
   if (!isVisibleTime(options.businessSettings)) return;
 
   // APPLY LOCALE
-  const detectedLocale = detectLocale(options)
-  options.locale = detectedLocale
+  options.locale = detectLocale(options);
 
   // APPLY CSS
-  await applyStyles(options)
+  await applyStyles(options);
 
-  // INITIALIZE
-  await initWidget(options, window.innerWidth <= 768)
+  let isWidgetInitialized = false;
+
+  const renderWidget = async (isMobile) => {
+    if (!isWidgetInitialized) {
+      await initWidget(options, window.innerWidth <= 768);
+      isWidgetInitialized = true;
+    } else {
+      options.locale = detectLocale(options);
+      configureWidget(options, isMobile);
+    }
+  };
+
+  // Initial render
+  const isMobile = window.innerWidth <= 768;
+  await renderWidget(isMobile);
+
+  // RESIZE EVENT LISTENER
+  const handleResize = debounce(() => {
+    const newIsMobile = window.innerWidth <= 768;
+    renderWidget(newIsMobile);
+  }, 200);
+  window.addEventListener("resize", handleResize);
+
+  // LOCALE CHANGE DETECTION
+  const handleLocaleChange = () => {
+    const newLocale = detectLocale(options);
+    if (newLocale !== options.locale) {
+      options.locale = newLocale;
+      renderWidget(window.innerWidth <= 768);
+    }
+  };
+
+  window.addEventListener("popstate", handleLocaleChange);
+  window.addEventListener("localeChange", handleLocaleChange);
+
+  return () => {
+    window.removeEventListener("resize", handleResize);
+    window.removeEventListener("popstate", handleLocaleChange);
+    window.removeEventListener("localeChange", handleLocaleChange);
+  };
 }
 
 // Make the function available globally
--- widgetRenderer.js
+++ widgetRenderer.js
@@ -1,38 +1,44 @@
 export const initWidget = async (options, isMobile) => {
-  // message text replace alls
-  processMessageText(options)
+  processMessageText(options);
 
-  const htmlResponse = await fetch('https://wawidgetstorage.blob.core.windows.net/widget-assets/widget.html', { cache: 'no-store' });
-  const htmlText = await htmlResponse.text();
+  const existingWidget = document.getElementById("whatsapp-chat-widget");
 
-  document.body.insertAdjacentHTML('beforeend', htmlText);
+  if (!existingWidget) {
+    const htmlResponse = await fetch(
+      "https://wawidgetstorage.blob.core.windows.net/widget-assets/widget.html",
+      { cache: "no-store" },
+    );
+    const htmlText = await htmlResponse.text();
+    document.body.insertAdjacentHTML("beforeend", htmlText);
+  }
+
   configureWidget(options, isMobile);
-  addEventListeners();
-}
+  if (!existingWidget) addEventListeners();
+};
 
 const processMessageText = (options) => {
   if (options.businessSettings.messageText) {
     options.businessSettings.messageText = options.businessSettings.messageText
-      .replaceAll('{{page_link}}', encodeURIComponent(window.location.href))
-      .replaceAll('__page_link__', encodeURIComponent(window.location.href))
-      .replaceAll('{{page_title}}', window.document.title)
-      .replaceAll('__page_title__', window.document.title)
-      .replaceAll('\n', '%0A');
+      .replaceAll("{{page_link}}", encodeURIComponent(window.location.href))
+      .replaceAll("__page_link__", encodeURIComponent(window.location.href))
+      .replaceAll("{{page_title}}", window.document.title)
+      .replaceAll("__page_title__", window.document.title)
+      .replaceAll("\n", "%0A");
   }
 };
 
-const configureWidget = (options, isMobile) => {
+export const configureWidget = (options, isMobile) => {
   if (options.autoShow) {
     toggleChatBox(true);
   }
 
-  setWidgetTexts(options, isMobile)
-  setQRCode(options, isMobile)
+  setWidgetTexts(options, isMobile);
+  setQRCode(options, isMobile);
 };
 
 const setWidgetTexts = (options, isMobile) => {
-  const toastElement = document.getElementById('wa-widget-toast');
-  const toastTextElement = document.getElementById('wa-widget-toast-title')
+  const toastElement = document.getElementById("wa-widget-toast");
+  const toastTextElement = document.getElementById("wa-widget-toast-title");
   const titleElement = document.getElementById("widget-title");
   const subtitleElement = document.getElementById("widget-subtitle");
   const buttonElement = document.getElementById("widget-cta-title");
@@ -40,71 +46,89 @@ const setWidgetTexts = (options, isMobile) => {
 
   if (toastElement && toastTextElement) {
     if (!options.displaySettings.showToast) {
-      toastElement.remove()
-      return
+      toastElement.remove();
+      return;
     }
 
-    toastTextElement.textContent = options.displaySettings.translations[options.locale].toastTitle
+    toastTextElement.textContent =
+      options.displaySettings.translations[options.locale].toastTitle;
   }
 
-  if (titleElement) titleElement.textContent = options.displaySettings.translations[options.locale].title
-  if (subtitleElement) subtitleElement.textContent = isMobile ? options.displaySettings.translations[options.locale].mobileSubtitle : options.displaySettings.translations[options.locale].desktopSubtitle
+  if (titleElement)
+    titleElement.textContent =
+      options.displaySettings.translations[options.locale].title;
+  if (subtitleElement)
+    subtitleElement.textContent = isMobile
+      ? options.displaySettings.translations[options.locale].mobileSubtitle
+      : options.displaySettings.translations[options.locale].desktopSubtitle;
 
   if (buttonElement && buttonTextElement) {
-    if (options.displaySettings.showCTA === false && !isMobile) {
-      buttonElement.remove()
-      return
+    if (isMobile) {
+      buttonElement.style.display = "flex";
+      const phoneNumber = options.businessSettings.phoneNumber.replace(
+        /\+/g,
+        "",
+      );
+      const messageText =
+        options.businessSettings.translations[options.locale].message || "";
+      buttonElement.href = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${messageText}`;
+      buttonTextElement.textContent =
+        options.displaySettings.translations[options.locale].ctaTitle;
+
+      return;
     }
 
-    const phoneNumber = options.businessSettings.phoneNumber.replace(/\+/g, '');
-    const messageText = options.businessSettings.translations[options.locale].message || '';
-    buttonElement.href = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${messageText}`;
-    buttonTextElement.textContent = options.displaySettings.translations[options.locale].ctaTitle
+    if (options.displaySettings.showCTA === false) {
+      buttonElement.style.display = "none";
+    }
   }
-}
+};
 
 const setQRCode = (options, isMobile) => {
-  const qrCodeDiv = document.getElementById('wa-qr-code')
-  const qrCodeImg = document.getElementById('wa-qr-code-img')
+  const qrCodeDiv = document.getElementById("wa-qr-code");
+  const qrCodeImg = document.getElementById("wa-qr-code-img");
 
-  if (options.businessSettings.translations[options.locale].qrURL && !isMobile) {
+  if (isMobile) {
+    qrCodeDiv.style.display = "none";
+    return;
+  }
+
+  if (options.businessSettings.translations[options.locale].qrURL) {
     if (qrCodeDiv && qrCodeImg) {
-      qrCodeImg.src = options.businessSettings.translations[options.locale].qrURL;
+      qrCodeDiv.style.display = "block";
+      qrCodeImg.src =
+        options.businessSettings.translations[options.locale].qrURL;
     }
   } else {
     if (qrCodeDiv) {
-      qrCodeDiv.remove()
+      qrCodeDiv.remove();
     }
   }
-}
+};
 
 const toggleChatBox = (autoOpen) => {
   const elements = {
-    toast: document.getElementById('wa-widget-toast'),
-    chatBox: document.getElementById('wa-chat-box'),
-    openedSvg: document.querySelector('#wa-widget-opened-svg'),
-    bubble: document.querySelector('.wa-chat-bubble'),
-    sendButton: document.querySelector('.wa-widget-send-button')
+    toast: document.getElementById("wa-widget-toast"),
+    chatBox: document.getElementById("wa-chat-box"),
+    sendButton: document.querySelector(".wa-widget-send-button"),
   };
 
   if (!elements.chatBox) return;
 
-  const isVisible = elements.chatBox.classList.contains('wa-chat-box-visible');
+  const isVisible = elements.chatBox.classList.contains("wa-chat-box-visible");
   const shouldOpen = autoOpen || !isVisible;
 
-  elements.toast.classList.toggle('wa-widget-toast-hidden', shouldOpen)
-  elements.chatBox.classList.toggle('wa-chat-box-visible', shouldOpen);
-  elements.openedSvg.style.display = shouldOpen ? 'block' : 'none';
-  elements.bubble.style.cssText = shouldOpen ? 'display: none;' : '';
-  elements.sendButton.className = `wa-widget-send-button${shouldOpen ? ' wa-widget-send-button-clicked' : ''}`;
+  elements.toast.classList.toggle("wa-widget-toast-hidden", shouldOpen);
+  elements.chatBox.classList.toggle("wa-chat-box-visible", shouldOpen);
+  elements.sendButton.className = `wa-widget-send-button${shouldOpen ? " wa-widget-send-button-clicked" : ""}`;
 };
 
 const addEventListeners = () => {
-  const widget = document.getElementById('whatsapp-chat-widget');
+  const widget = document.getElementById("whatsapp-chat-widget");
   if (!widget) return;
 
-  widget.addEventListener('click', function(event) {
-    if (event.target.closest('.wa-widget-send-button')) {
+  widget.addEventListener("click", function (event) {
+    if (event.target.closest(".wa-widget-send-button")) {
       toggleChatBox();
     }
   });
